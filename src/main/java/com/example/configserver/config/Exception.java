8cf1e47308cb0f204ebee350c8db0bfed082669d
Orlando Kuan <orlandojose.kuanbecerra@everis.nttdata.com>
2021-08-11 16:02:55 -0500

    REFACTOR exception

--- src/main/java/com/example/configserver/config/Exception.java
+++ src/main/java/com/example/configserver/config/Exception.java
@@ -1,4 +1,13 @@
 package com.example.configserver.config;
 
 public class Exception {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
